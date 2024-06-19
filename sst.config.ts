86a94b544f1b63097704977df66ee887f72cb0ed
Matthew Wyskiel <matt@mattwyskiel.com>
2024-06-19 19:59:51 -0400

    Add more defaults to API routes, reference global event bus, use filesystem to read event schemas instead of TS Imports

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,5 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-import * as NewMessageSchema from './events/schemas/NewMessage.json';
+import { readFileSync } from 'fs';
 export default $config({
   app(input) {
     return {
@@ -15,30 +15,39 @@ export default $config({
   },
   async run() {
     const bucket = new sst.aws.Bucket('EmailBucket');
-    const api = new sst.aws.ApiGatewayV2('EmailApi');
+    const eventBus = aws.cloudwatch.EventBus.get('EventBus', 'whiskey-event-bus');
+    const api = new sst.aws.ApiGatewayV2('EmailApi', {
+      transform: {
+        route: {
+          handler: {
+            permissions: [
+              {
+                actions: ['events:PutEvents'],
+                resources: [eventBus.arn],
+              },
+            ],
+            link: [bucket],
+          },
+        },
+      },
+    });
 
     const newMessageSchema = new aws.schemas.Schema('NewMessageSchema', {
       registryName: 'com.mattwyskiel.whiskey.events',
       name: 'whiskey.email.NewMessage',
       description: 'A new email message received by *@mattwyskiel.com or mwwyskiel@gmail.com',
       type: 'OpenApi3',
-      content: JSON.stringify(NewMessageSchema),
+      content: readFileSync('./events/schemas/NewMessage.json', 'utf8'),
     });
 
-    const improvmx = api.route('POST /improvmx-webhook', {
-      handler: 'backend/functions/improvmx-webhook.handler',
-      link: [bucket],
-    });
-    bucket.subscribe(
-      {
-        handler: 'backend/functions/on-improvmx-message-add.handler',
-        link: [bucket],
-      },
-      {
-        events: ['s3:ObjectCreated:*'],
-        filterPrefix: 'raw/improvmx/',
-      }
+    const improvmx = api.route(
+      'POST /improvmx-webhook',
+      'backend/functions/improvmx-webhook.handler'
     );
+    bucket.subscribe('backend/functions/on-improvmx-message-add.handler', {
+      events: ['s3:ObjectCreated:*'],
+      filterPrefix: 'raw/improvmx/',
+    });
 
     const addToImprovMXDomain = new sst.aws.Function('AddToImprovMXDomain', {
       handler: 'backend/functions/add-to-improvmx-domain.handler',
@@ -52,10 +61,7 @@ export default $config({
     });
 
     const gmailTopic = new gcp.pubsub.Topic('GmailTopic');
-    api.route('POST /gmail-webhook', {
-      handler: 'backend/functions/gmail-webhook.handler',
-      link: [bucket],
-    });
+    api.route('POST /gmail-webhook', 'backend/functions/gmail-webhook.handler');
     const gmailSubscription = new gcp.pubsub.Subscription('GmailWebhookSubscription', {
       topic: gmailTopic.id,
       pushConfig: {
@@ -76,15 +82,9 @@ export default $config({
       { dependsOn: [gmailSubscription] }
     );
 
-    bucket.subscribe(
-      {
-        handler: 'backend/functions/on-gmail-message-add.handler',
-        link: [bucket],
-      },
-      {
-        events: ['s3:ObjectCreated:*'],
-        filterPrefix: 'raw/gmail/',
-      }
-    );
+    bucket.subscribe('backend/functions/on-gmail-message-add.handler', {
+      events: ['s3:ObjectCreated:*'],
+      filterPrefix: 'raw/gmail/',
+    });
   },
 });
