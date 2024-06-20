1c083e5efa5fa4b9815b64fedfa7de5363bb5ab0
Matthew Wyskiel <matt@mattwyskiel.com>
2024-06-19 20:56:04 -0400

    chore: Refactor database connection to use Secrets class

--- backend/db/index.ts
+++ backend/db/index.ts
@@ -1,10 +1,11 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import * as schema from './schema';
 import postgres from 'postgres';
-import { getSecret } from '../services/secrets';
+import { Secrets } from '../services/secrets';
 
 export const db = async () => {
-  const connectionString = (await getSecret('DATABASE_CONNECTION')).secretValue;
+  const secrets = new Secrets();
+  const connectionString = await secrets.get('DATABASE_CONNECTION');
   const client = postgres(connectionString, { prepare: false });
   return drizzle(client, { schema });
 };
